fb95717916937685550c8449d1eca324789be5a3
liuhe <liuhe@xylink.cn>
2020-04-14 13:40:07 +0800

    demo

--- app/src/main/java/me/liuhe92/ui/MainActivity.java
+++ app/src/main/java/me/liuhe92/ui/MainActivity.java
@@ -3,8 +3,13 @@ package me.liuhe92.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import me.liuhe92.R;
+import me.liuhe92.httputils.OkHttpUtils;
+import me.liuhe92.httputils.callback.StringCallback;
+import okhttp3.Call;
+import okhttp3.Request;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onGetClick(View view) {
+        String url = "http://www.csdn.net/";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .addParams("username", "hyman")
+                .addParams("password", "123")
+                .build()
+                .execute(new StringCallback() {
+
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("123", "12err");
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("123", response);
+                    }
+                });
     }
 }
